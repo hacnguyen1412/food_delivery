abcc9c9d53b1dfb1e5df62d0f8e3a38217956028
Hac Nguyen <nguyentranhoanghac@gapo.com.vn>
2023-01-02 20:21:03 +0700

    remove trash code

--- core/core_realm/lib/src/database.dart
+++
@@ -1,6 +0,0 @@
-import 'package:core_realm/core_realm.dart';
-
-abstract class Database {
-  CoreRealm? get appRealm;
-  Configuration get config;
-}
--- features/profile_feature/lib/src/data/profile/model/model.dart
+++ features/profile_feature/lib/src/data/profile/model/model.dart
@@ -1,16 +1,1 @@
-import 'package:core_package/common/model.dart';
 
-class ProfileModel extends DatabaseModel {
-  final String name;
-  @override
-  factory ProfileModel.fromJson(Map<String, dynamic> json) {
-    return ProfileModel(name: json['name']);
-  }
-
-  ProfileModel({required this.name}) : super(null);
-
-  @override
-  Map<String, dynamic> toJson() {
-    return {'name': name};
-  }
-}
